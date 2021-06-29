1a3b171e19cf85c028b47429a2c4bda3b4a4f550
Alex <darurium@gmail.com>
2021-06-29 07:58:27 +0500

    first

---
+++ script.js
@@ -0,0 +1,14 @@
+const getResource = async (url) => {
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`Could not fetch url: ${url}`);
+    }
+
+    const some = await res.json();
+
+    return some;
+}
+
+getResource('https://jsonplaceholder.typicode.com/posts/1000')
+    .then((res) => console.log("Success", res))
+    .catch((error) => console.error("Error", error));
